a0ae7abf66ee4ea250c690f1ff2d25144165b781
martin-samson-png <martinsams64@gmail.com>
2025-04-30 16:45:22 +0200

    feat : ajout de la section comments

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 const main = document.createElement("main");
+const sectionComments = document.createElement("section");
+const commentsContainer = document.createElement("div")
+sectionComments.id = "sectionComments";
+commentsContainer.id = "divComments"
 document.body.appendChild(main);
+main.appendChild(sectionComments);
+sectionComments.appendChild(commentsContainer)
+
+async function commentsFetchData() {
+    try{
+        const commentsResponse = await fetch("https://jsonplaceholder.typicode.com/comments");
+        if (!commentsResponse.ok){
+            throw new Error ("Network response was not ok");
+        }
+        const commentsData = await commentsResponse.json();
+        console.log(commentsData);
+        commentsData.forEach(comments => {
+            const commentsElement = document.createElement("article");
+            commentsElement.innerHTML = `
+            <h3>${comments.name}</h3>
+            <p>${comments.body}</p>`;
+            commentsContainer.appendChild(commentsElement);
+        });
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
+commentsFetchData();
